6bbca0cc258826da5df0d8d336429d637ea04741
Jess <jess.harrelson0707@gmail.com>
2016-12-04 01:16:36 -0500

    working with adding artists to db and associating them to users

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,10 +1,31 @@
 class ArtistsController < ApplicationController
   skip_before_action :authenticate_user
+  # def create
+  #   # check out params, they are awesome
+  #   #  save this data to artist tabel in db
+  #   # render back a conformatin that data was saved
+  # end
+
   def create
-    # check out params, they are awesome
-    #  save this data to artist tabel in db
-    # render back a conformatin that data was saved
-    byebug
+   byebug
+    # user_data:{id: id, artists: [{artist_data}, {artist_data}, {artist_data}]}
+    # make sure artist id is an integer
+    user = User.find(user_data.id)
+    # make sure artists array is coming into here
+    liked_artists = []
+      artists.each do |artist|
+      # make sure the keys and values are correct below
+      new_artist = Artist.find_or_create_by(artist_spotify_id: artist.id, name: artist.name, image: artist.image)
+      User.artist = new_artist
+      liked_artists.push(new_artist)
+      end
+    liked_artists
+  end
+
+  private
+  # something is off with the params and passing this crap in :(
+  def artists_params
+    params.require(:artists).permit(:artistsData)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,12 @@ class UsersController < ApplicationController
     user = User.new(user_params)
 
     if user.save
-     jwt = Auth.issue({user_id: user.id})
-     email = user.email
-     phone_number = user.phone_number
-     render json: {jwt: jwt, userEmail: email, userPhoneNumber: phone_number}
+
+      jwt = Auth.issue({user_id: user.id})
+      email = user.email
+      phone_number = user.phone_number
+      id = user.id
+      render json: {jwt: jwt, userId: id, userEmail: email, userPhoneNumber: phone_number}
 
    else
      render json: {error: 'email is not unique'}
@@ -30,7 +32,7 @@ private
 
   def user_params
     # this is strong params
-    params.require(:user).permit(:email, :password, :phone_number)
+    params.require(:user).permit(:email, :password, :phone_number, :artists)
     # changed :user from :auth
   end
 
