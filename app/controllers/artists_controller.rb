4dca07b131a0973984b71d98d5eac82ba9f274ba
Jess <jess.harrelson0707@gmail.com>
2016-12-05 13:25:40 -0500

    3 fav artists associated with user and artist and saved to db. Returns an array of liked artists. Worked in artists controller create method

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,5 @@
 class ArtistsController < ApplicationController
-  skip_before_action :authenticate_user
+  # skip_before_action :authenticate_user
   # def create
   #   # check out params, they are awesome
   #   #  save this data to artist tabel in db
@@ -7,30 +7,50 @@ class ArtistsController < ApplicationController
   # end
 
   def create
-   byebug
-   artist1 = params["artistsData"]["artist1"]["artist_spotify_id"]
-   artist2 = params["artistsData"]["artist2"]
-   artist3 = params["artistsData"]["artist3"]
-    # user_data:{id: id, artists: [{artist_data}, {artist_data}, {artist_data}]}
-    # make sure artist id is an integer
-    user = User.find(user_data.id)
-    # make sure artists array is coming into here
+    artist1 = {}
+    artist2 = {}
+    artist3 = {}
+
+   artist1["artist_spotify_id"] = params["artists"]["artistsData"][0]["artist_spotify_id"]
+   artist1["name"] = params["artists"]["artistsData"][0]["name"]
+   artist1["image"] = params["artists"]["artistsData"][0]["image"]
+
+   artist2["artist_spotify_id"] = params["artists"]["artistsData"][1]["artist_spotify_id"]
+   artist2["name"] = params["artists"]["artistsData"][1]["name"]
+   artist2["image"] = params["artists"]["artistsData"][1]["image"]
+
+   artist3["artist_spotify_id"] = params["artists"]["artistsData"][2]["artist_spotify_id"]
+   artist3["name"] = params["artists"]["artistsData"][2]["name"]
+   artist3["image"] = params["artists"]["artistsData"][2]["image"]
+
+   artists = [artist1, artist2, artist3]
+
+    # byebug
+
+    user = User.find(current_user.id)
+
     liked_artists = []
+
+    # make sure artists array is coming into here
       artists.each do |artist|
-      # make sure the keys and values are correct below
-      new_artist = Artist.find_or_create_by(artist_spotify_id: artist.id, name: artist.name, image: artist.image)
-      User.artist = new_artist
+        # byebug
+      new_artist = Artist.find_or_create_by(artist_spotify_id: artist["artist_spotify_id"], name: artist["name"], image: artist["image"])
+
+      UserArtist.find_or_create_by(artist_id: new_artist.id, user_id: user.id)
+
       liked_artists.push(new_artist)
       end
-    liked_artists
+      # byebug
+    render json: {liked_artists: liked_artists}
   end
 
-  private
-  # something is off with the params and passing this crap in :(
-  def artists_params
-    # params.require(:artists).permit(:artistsData)
-    # params.require(:artists).permit(artistsData: [ artist1: [:artist_spotify_id, :name, :image], artist2: [:artist_spotify_id, :name, :image], artist3: [:artist_spotify_id, :name, :image] ])
-    params.require(:artists).permit(:artistsData => [:artist1 => {:artist_spotify_id :name :image} :artist2 => {:artist_spotify_id :name :image} :artist3 => {:artist_spotify_id :name :image}])
-  end
+  # private
+  # # something is off with the params and passing this crap in :(
+  # def artists_params
+  #   byebug
+  #   # params.require(:artists).permit(:artistsData)
+  #   # params.require(:artists).permit(artistsData: [ artist1: [:artist_spotify_id, :name, :image], artist2: [:artist_spotify_id, :name, :image], artist3: [:artist_spotify_id, :name, :image] ])
+  #   params.require(:artists).permit(:artistsData => [:artist1 => [:artist_spotify_id, :name, :image], :artist2 => [:artist_spotify_id, :name, :image], :artist1 => [:artist_spotify_id, :name, :image]])
+  # end
 
 end
